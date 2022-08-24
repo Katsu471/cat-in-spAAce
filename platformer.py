0d541f8c13d95f6588a9e5e6ad330da2e659b49b
Katsu471 <76861480+Katsu471@users.noreply.github.com>
2022-08-24 21:06:00 +0500

    py file

---
+++ platformer.py
@@ -0,0 +1,226 @@
+import pygame, sys # import pygame and sys
+
+clock = pygame.time.Clock() # set up the clock
+
+from pygame.locals import * # import pygame modules
+#from pygame_functions import * #i need to get this library and search for others too
+
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init() # initiate pygame
+
+pygame.display.set_caption("cat in spaAaAaAaAaAAAaaaCEeee") # set the window name
+
+WINDOW_SIZE = (800,600) # set up window size
+
+screen = pygame.display.set_mode(WINDOW_SIZE,0,32) # initiate screen
+
+display = pygame.Surface((300, 200))
+
+
+
+def generate_map(path): #Function to generate game map from text file, the text file has a bunch of numbers arranged like how the map should be
+    f = open (path + ".map","r") #ease of use XD
+    data = f.read()
+    f.close()
+    data = data.split("\n")
+    game_map = []
+    for row in data:
+        game_map.append(list(row))
+    return game_map
+
+game_map = generate_map("Data//maps//testlevel")
+
+def collision_test(rect, tiles):
+    hit_list = []
+    for tile in tiles:
+        if rect.colliderect(tile):
+            hit_list.append(tile)
+    return hit_list
+
+def move(rect, movement, tiles):
+    collision_types = {"top": False, "bottom": False, "right": False, "left": False}
+    rect.x += movement[0]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[0] > 0:
+            rect.right = tile.left
+            collision_types["right"] = True
+        elif movement[0] < 0:
+            rect.left = tile.right
+            collision_types["left"] = True
+    rect.y += movement[1]
+    hit_list = collision_test(rect, tiles)
+    for tile in hit_list:
+        if movement[1] > 0:
+            rect.bottom = tile.top
+            collision_types["bottom"] = True
+        elif movement[1] < 0:
+            rect.top = tile.bottom
+            collision_types["top"] = True
+    return rect, collision_types
+
+
+
+player_image = pygame.image.load("Data//images//player//cat.png")
+player_image_left = pygame.image.load("Data//images//player//cat_left.png")
+player_image_left.set_colorkey((255, 255, 255))
+player_image.set_colorkey((255, 255, 255))
+
+
+grass_image = pygame.image.load("Data//images//grass.png")
+TILE_SIZE = grass_image.get_width()
+blank_dirt = pygame.image.load("Data//images//BlankDirt.png")
+dirt_image = pygame.image.load("Data//images//dirt.png")
+
+#bACKGROUND STyFF
+
+#STAR LAYERS START (1 = most far,lowest multiplayer. 5 = closest highest multiplayer)
+stars_layer_1 = pygame.image.load("Data//images//Background//star_layer_1.png")
+
+stars_layer_2 = pygame.image.load("Data//images//Background//star_layer_2.png")
+
+stars_layer_3 = pygame.image.load("Data//images//Background//star_layer_3.png")
+
+
+stars_layer_4 = pygame.image.load("Data//images//Background//star_layer_4.png")
+
+stars_layer_5 = pygame.image.load("Data//images//Background//star_layer_5.png")
+
+Nebula_1 = pygame.image.load("Data//images//Background//Nebula_1.png")
+
+
+#(sounds
+jump_sfx = pygame.mixer.Sound("Data/sound/jump_sfx.wav")
+jump_sfx.set_volume(0.7)
+pygame.mixer.music.load("Data/sound/bgmcat.ogg")
+pygame.mixer.music.play(-1)
+#)
+
+
+
+
+moving_right = False
+moving_left = False
+
+player_y_momentum = 0
+air_timer = 0
+
+true_scroll = [0,0]
+
+player_rect = pygame.Rect(50, 50, player_image.get_width(), player_image.get_height())
+test_rect = pygame.Rect(100,100,100,50)
+
+Faceing = 0
+
+background_objects = [[0.005,[0,0,300,200]], [0.005,[300,0,300,200]], [0.005,[600,0,300,200]], [0.01,[0,0,300,200]], [0.01,[300,0,300,200]], [0.01,[600,0,300,200]], [0.10, [0,0,300,200]], [0.10, [300,0,300,200]], [0.10, [600,0,300,200]], [0.11, [150,0,400,400]], [0.15, [0,0,300,200]], [0.15, [300,0,300,200]], [0.15, [600,0,300,200]], [0.15, [900,0,300,200]], [0.20, [0,0,300,200]], [0.20, [300,0,300,200]], [0.20, [600,0,300,200]], [0.20, [900,0,300,200]]] #CONTINUE FROM HERE, MAKE STARS, MAKE NEBULI, MAKE FLOATING DEBRIS, MAKE STONES, DONE
+
+
+while True: # game loop
+    display.fill((0,5,18)) #use 0,5,17 for the actual value
+
+    true_scroll[0] += (player_rect.x - true_scroll[0]-148)/10
+    true_scroll[1] += (player_rect.y - true_scroll[1]-106)/10
+    scroll = true_scroll.copy()
+    scroll[0] = int(scroll[0])
+    scroll[1] = int(scroll[1])
+    #background objects
+    for background_object in background_objects:
+        obj_rect = pygame.Rect(background_object[1] [0]-scroll[0]*background_object[0], background_object[1] [1]-scroll[1]*background_object[0], background_object[1] [2], background_object[1] [3]) #use vars next time this feels like programming in BF :skull:
+        
+        if background_object[0] == 0.005:
+            display.blit(stars_layer_1, obj_rect)
+        
+        if background_object[0] == 0.01:
+            display.blit(stars_layer_2, obj_rect)
+        
+        if background_object[0] == 0.10:
+            display.blit(stars_layer_3, obj_rect)
+        
+        if background_object[0] == 0.11:
+            display.blit(Nebula_1, obj_rect)
+        
+        if background_object[0] == 0.15:
+            display.blit(stars_layer_4, obj_rect)
+        
+        if background_object[0] == 0.20:
+            display.blit(stars_layer_5, obj_rect)
+
+        if background_object[0] == 0.5:
+            pygame.draw.rect(display, (244,146,140), obj_rect)
+
+
+
+
+    tile_rects = []
+    y = 0
+    for row in game_map:
+        x = 0
+        for tile in row:
+            if tile == "1":
+                display.blit(dirt_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile == "2":
+                display.blit(grass_image, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile == "3":
+                display.blit(blank_dirt, (x * TILE_SIZE - scroll[0], y * TILE_SIZE - scroll[1]))
+            if tile != "0":
+                tile_rects.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
+            x += 1
+        y += 1
+
+    player_movement = [0, 0]
+    if moving_right:
+        player_movement[0] += 2
+        Faceing = 0
+    if moving_left:
+        player_movement[0] -= 2
+        Faceing = 1
+    player_movement[1] += player_y_momentum
+    player_y_momentum += 0.2
+    if player_y_momentum > 3:
+        player_y_momentum = 3
+
+    player_rect, collisions = move(player_rect, player_movement, tile_rects)
+
+    if collisions["bottom"]:
+        player_y_momentum = 0
+        air_timer = 0
+    else:
+        air_timer += 1
+
+    if Faceing == 1:
+        display.blit(player_image_left, (player_rect.x - scroll[0], player_rect.y - scroll[1]))
+    else: 
+        display.blit(player_image, (player_rect.x - scroll[0], player_rect.y - scroll [1]))
+
+    for event in pygame.event.get(): # event loop
+        if event.type == QUIT: # check for window quit
+            pygame.quit() # stop pygame
+            sys.exit() # stop script
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                moving_right = True
+            if event.key == K_LEFT:
+                moving_left = True
+            if event.key == K_UP:
+                if air_timer < 6:
+                    player_y_momentum = -3
+                    jump_sfx.play()
+        if event.type == KEYUP:
+            if event.key == K_RIGHT:
+                moving_right = False
+            if event.key == K_LEFT:
+                moving_left = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x = event.pos[0]
+            y = event.pos[1]
+            
+            if event.button == 1:
+                print(f"Left click! x = {x} and y = {y}")
+
+
+    surf = pygame.transform.scale(display, WINDOW_SIZE)
+    screen.blit(surf, (0, 0))
+    pygame.display.update() # update display
+    clock.tick(60) # maintain 60 fps or any id assigned9
